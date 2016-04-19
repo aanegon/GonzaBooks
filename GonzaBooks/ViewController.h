8f3fcfec6a4bb80d2823d64070a40d5a02493502
Simed <simed@macbook-pro-de-miguel.local>
2016-04-19 09:29:16 +0200

    Initial Commit

---
+++ GonzaBooks/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GonzaBooks
+//
+//  Created by Simed on 19/4/16.
+//  Copyright © 2016 Simed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GonzaBooks/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GonzaBooks
+//
+//  Created by Simed on 19/4/16.
+//  Copyright © 2016 Simed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
